4419a063e907fcfe07a495228e85e3cbdffc8684
Muhammad Sameer <itsmdsameerkhan@gmail.com>
2020-06-16 20:48:52 +0545

    Updates

---
+++ precache-manifest.b58a4c3374d0ca06436cf899a539b16e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb6cbdbb5823bfe4dc53d4d61fc8146e",
+    "url": "/cooltube/index.html"
+  },
+  {
+    "revision": "254d42f73814df0984db",
+    "url": "/cooltube/static/css/main.79242ed4.chunk.css"
+  },
+  {
+    "revision": "a8730c1978c49ede28e6",
+    "url": "/cooltube/static/js/2.36a94e28.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/cooltube/static/js/2.36a94e28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "254d42f73814df0984db",
+    "url": "/cooltube/static/js/main.3ca89d83.chunk.js"
+  },
+  {
+    "revision": "a8db1ebb6ac28a0527b3",
+    "url": "/cooltube/static/js/runtime-main.15be81bc.js"
+  }
+]);
